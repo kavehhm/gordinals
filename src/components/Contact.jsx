d8e595c68cb73fee42d34abfa67419edd8dc6448
wuavonft <kavehm013@gmail.com>
2023-06-03 20:03:55 -0700

    ain

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -78,9 +78,9 @@ export default function Contact() {
 
        <footer className="bg-transparent">
       <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-12 lg:px-8">
-        <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
+        <nav className="-mb-6 mx-auto align-middle justify-items-center columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
           {navigation.main.map((item) => (
-            <div key={item.name} className="pb-6">
+            <div key={item.name} className="pb-6 text-center">
               <a href={item.href} className="text-lg leading-6 text-gray-200 hover:underline">
                 {item.name}
               </a>
